e17dd6a13c9a3682c000fdb3aa451e6d76230f12
sumanmandal1992 <mandalsuman62@gmail.com>
2022-06-27 15:33:01 +0530

    test file removed

--- test.js
+++
@@ -1,6 +0,0 @@
-const dt = new Date('2022-01-01');
-const year = dt.getFullYear();
-const month = String(dt.getMonth()).padStart(2, 0);
-const day = String(dt.getDate()).padStart(2, 0)
-const date = `${year}-${month}-${day}`;
-console.log(date);
